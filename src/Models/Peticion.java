a7336985a2811f7cf96e81a033dd4d5ac18ddbdd
IgnacioF20 <fontanaignacio@gmail.com>
2021-10-20 16:19:22 -0300

    test

---
+++ src/Models/Peticion.java
@@ -0,0 +1,12 @@
+package Models;
+
+import java.util.Date;
+
+public class Peticion {
+    private String obraSocial;
+    private Date fechaCarga;
+    private Date fechaEntrega;
+    private boolean finalizada;
+
+
+}
