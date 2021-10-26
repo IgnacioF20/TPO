403979e65e4af8881365f9eb1d2c54f85586c6e9
Ivan Pace <ipace@uade.edu.ar>
2021-10-26 20:31:26 -0300

    Agregando CargoEnum, PacienteController, PacienteDTO y SexoEnum

--- src/Controllers/pacienteController.java
+++ src/Controllers/pacienteController.java
@@ -1,4 +1,9 @@
 package Controllers;
 
 public class pacienteController {
+    private ArrayList<Paciente> listaDePaciente = new ArrayList();
+
+    public void NuevoPaciente(PacienteDTO p){
+        this.listaDePaciente.add(p.toObjet());
+    }
 }
