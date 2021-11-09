324206bf2ec9d407e201acf1029f05727184c13c
Ivan Pace <ipace@uade.edu.ar>
2021-11-09 20:26:46 -0300

    Creating methods

--- src/com/company/controllers/LaboratorioController.java
+++ src/com/company/controllers/LaboratorioController.java
@@ -1,26 +1,24 @@
 package com.company.controllers;
 
 import com.company.dto.SucursalDTO;
+import com.company.models.Peticion;
 import com.company.models.Sucursal;
 import java.util.ArrayList;
 
 public class LaboratorioController {
 
     ArrayList<Sucursal> sucursales = new ArrayList();
+    ArrayList<Peticion> peticiones = new ArrayList();
 
 
     public boolean altaSucursal(SucursalDTO sucursal){
-
-        // El numero de sucursal se asigna automaticamente al generarse la sucursal.
-        // El identificador de duplicado puede ser la direccion de la sucursal.
-        if(estaCargada(sucursal.getDirección())){
+        if(obtenerSucursal(sucursal.getNumero()) != null){
             this.sucursales.add(dtoToModel(sucursal));
             return true;
         }
         else
             return false;
     }
-
 
     private Sucursal obtenerSucursal(int nroSucursal){
         Sucursal sucursalBuscada = null;
@@ -35,9 +33,7 @@ public class LaboratorioController {
 
 
     private boolean estaCargada(String direccion){
-
         boolean cargada = false;
-
         for(Sucursal sucursal : sucursales ){
             if(sucursal.getDirección() == direccion){
                 cargada = true;
@@ -46,21 +42,12 @@ public class LaboratorioController {
         }
         return cargada;
     }
-
-
     private Sucursal dtoToModel(SucursalDTO sucursal){
         Sucursal sucursalNueva = new Sucursal(sucursal.getNumero(), sucursal.getDirección(), sucursal.getResponsableTecnico());
         return sucursalNueva;
     }
-
-
     public boolean modificarSucursal(SucursalDTO sucursal){
-
-
-//        Como modificar sucursal porque el DTO no debería incluir el numero de sucursal
-//        Numero de sucursal es un id
         Sucursal sucursalAEditar = obtenerSucursal(sucursal.getNumero());
-
         if(sucursalAEditar != null){
             sucursalAEditar.setDirección(sucursal.getDirección());
             sucursalAEditar.setNumero(sucursal.getNumero());
@@ -70,19 +57,17 @@ public class LaboratorioController {
         else
             return false;
     }
-
-
-    //IMPORTANTE
-    //como hacer para obtener las peticiones de las sucursales
-    // tenemos 2 array list? o vamos directamente buscando por el Paciente?
-    // como se manejan las listas de agregacion?
     public boolean tienePeticionesActivas(int nroSucursal){
         Sucursal sucursal = obtenerSucursal(nroSucursal);
         if(sucursal == null){
-            //OBTENER las peticiones de las sucursales
-            ;
+            for(Peticion peticion : peticiones ){
+                if(peticion.estaActiva()){
+                    return true;
+                    break;
+                }
+            }
         }
-        return true;
+        return false;
     }
 
     // IMPORTANTE
