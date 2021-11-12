f4e84139f2e599cc4909a35ae438bed4bd823c4d
feder <federica.moro.23@gmail.com>
2021-11-12 18:21:23 -0300

    Views

--- src/vistas/FrmLogIn.java
+++ src/vistas/FrmLogIn.java
@@ -1,15 +1,36 @@
 package vistas;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 public class FrmLogIn extends JFrame {
     private JPanel JPanel;
-    private JTextField textField1;
-    private JTextField textField2;
+    private JTextField txtDNI;
     private JButton iniciarSesiónButton;
+    private JPasswordField jpassPassword;
+    private  FrmPantallaPrincipal self;
+    char clave[]=jpassPassword.getPassword();
+    String clavedef=new String(clave);
 
     public FrmLogIn (String titulo){
+
         super(titulo);
+
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (UnsupportedLookAndFeelException e) {
+            e.printStackTrace();
+        }
+
 
         this.setContentPane(JPanel);
         this.setSize(400,400);
@@ -18,16 +39,48 @@ public class FrmLogIn extends JFrame {
         this.asociarEventos();
         this.setResizable(false);
 
+
+
+
+        iniciarSesiónButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                if (txtDNI.getText().equals("admin") && clavedef.equals("12345")){
+                    FrmLogIn.super.dispose();
+                    JOptionPane.showMessageDialog(null, "Bienvenido\n Has ingresado "
+                                    + "satisfactoriamente al sistema", "Mensaje de bienvenida",
+                            JOptionPane.INFORMATION_MESSAGE);
+                    FrmPantallaPrincipal frame = new FrmPantallaPrincipal(self,"Pantalla Principal");
+                    frame.setVisible(true);
+
+                }
+                else{
+
+                    JOptionPane.showMessageDialog(null, "Acceso denegado:\n"
+                                    + "Por favor ingrese un usuario y/o contraseña correctos",
+                            "Acceso denegado", JOptionPane.ERROR_MESSAGE);
+                }
+            }
+        });
+
+
+
+
     }
+
+
+
+
     private void asociarEventos()
     {
 
     }
 
+    public static void main(String[] args) {
 
-    private void createUIComponents() {
-        // TODO: place custom component creation code here
+        FrmLogIn frame = new FrmLogIn("Sistema de Laboratorio");
+        frame.setVisible(true);
     }
-
 
 }
