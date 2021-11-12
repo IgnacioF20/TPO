1b9644e694354f2dda448f328a399bd9a30b8a99
feder <federica.moro.23@gmail.com>
2021-11-12 18:15:31 -0300

    Views

--- src/vistas/FrmPantallaPrincipal.java
+++ src/vistas/FrmPantallaPrincipal.java
@@ -1,35 +1,43 @@
 package vistas;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class FrmPantallaPrincipal extends JFrame {
+public class FrmPantallaPrincipal extends JDialog {
 
     private JPanel JPanel;
     private JPanel pnlMain;
     private JPanel pnlUsuarios;
-    private JButton button1;
-    private JButton button2;
-    private JButton button3;
-    private JButton button4;
+    private JButton administrarUsuariosButton;
+    private JButton administrarSucursalesButton;
+    private JButton administrarPacientesButton;
+    private JButton consultarPeticionesButton;
     private JPanel pnlPacientes;
     private JPanel pnlPeticiones;
-    private JButton button5;
-    private JButton button6;
+    private JButton administrarPeticionesButton;
+    private JButton consultarPeticionesButton1;
     private JPanel pnlLaboratorio;
-    private JButton button7;
-    private JButton button8;
+    private JButton aministrarPracticasButton;
+    private JButton cargarResultadosButton;
     private JButton button9;
     private JButton button10;
+    private JButton btnSalir;
 
-    public FrmPantallaPrincipal(String titulo){
-        super(titulo);
+
+
+    public FrmPantallaPrincipal(Window owner, String titulo){
+        super(owner,"Pantalla Principal");
 
         this.setContentPane(JPanel);
         this.setSize(400,400);
+        this.setModal(true); //no permite volver a la pantalla anterior hasta cerrar la actual
         this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE); //establezco comportamiento al cerrarse
         this.setLocationRelativeTo(null); //que la pantalla inicie centrada
         this.asociarEventos();
         this.setResizable(false);
+
 
     }
     private void asociarEventos()
